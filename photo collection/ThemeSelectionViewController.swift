170b5cb1a0b9a5d3cb6e3ef8a65e6aaa2525978f
Stephanie Bowles <sbowl001@fiu.edu>
2019-05-17 21:51:02 +0800

    theme selection init setup

---
+++ photo collection/ThemeSelectionViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ThemeSelectionViewController.swift
+//  photo collection
+//
+//  Created by Stephanie Bowles on 5/17/19.
+//  Copyright © 2019 Stephanie Bowles. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    var themeHelper: ThemeHelper?
+    
+    
+    @IBAction func selectDarkTheme(_ sender: Any) {
+    }
+    
+    @IBAction func selectLightTheme(_ sender: Any) {
+    }
+    
+
+}
